91d950082913239388678570a320ed79ff0ddb6a
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2026-02-09 00:13:28 -0600

    feat: adding tanstack query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/shared/custom/components";
 import "./index.css";
 import App from "./App.tsx";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +20,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        {import.meta.env.MODE === 'development' && (<ReactQueryDevtools initialIsOpen={false} />)}
         <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
           <App />
         </ThemeProvider>
